1849db5faf75e54fb1ab0f6af4d619534166cc90
mattia-wsl <mat.ronzoni@gmail.com>
2023-07-22 00:03:33 +0200

    add directions

---
+++ src/components/Directions.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container
+    class="d-flex flex-column justify-center align-center"
+    style="background-color: rgba(94, 119, 69, 0.7)"
+  >
+    <h3>
+      Indicazioni
+      <v-icon icon="fas fa-route" size="small" class="ml-4"> </v-icon>
+    </h3>
+    <v-divider
+      thickness="2"
+      class="border-opacity-50 my-2"
+      style="width: 100%"
+    ></v-divider>
+    <v-btn
+      v-for="direction in directions"
+      block
+      variant="outlined"
+      :href="direction.link"
+      class="my-2"
+      v-bind:key="direction.title"
+    >
+      {{ direction.title }}
+    </v-btn>
+  </v-container>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      directions: [
+        {
+          title: "Dov'è la sposa?",
+          link: "https://www.google.com/maps?saddr=My+Location&daddr=Via+Aldo+Moro+41b+Treviolo",
+        },
+        {
+          title: "Dov'è lo sposo?",
+          link: "https://www.google.com/maps?saddr=My+Location&daddr=Via+Lungobrembo+18+Curno",
+        },
+        {
+          title: "Dov'è il matrimonio?",
+          link: "https://www.google.com/maps?saddr=My+Location&daddr=Chiesa+Marigolda",
+        },
+        {
+          title: "Dov'è il ricevimento?",
+          link: "https://www.google.com/maps?saddr=My+Location&daddr=Agriturismo+Larice+Clusone",
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,10 @@
       :height="screenHeight"
       aspect-ratio="4/3"
       cover
-      src="../../foto.JPG"
+      src="/foto.jpg"
       style="opacity: 0.7"
     >
-      <div
-        class="d-flex flex-column fill-height justify-center align-center text-gray"
-      >
+      <div class="d-flex fill-height justify-center align-end text-gray pb-4">
         <div
           style="
             background-color: azure;
@@ -21,33 +19,23 @@
         >
           <h1 class="text-h4 font-weight-bold mb-4">Lisa & Mattia</h1>
           <h4 class="subheading">31 Maggio 2024</h4>
+          <v-icon
+            icon="fas fa-angles-down"
+            size="large"
+            class="mt-4 moving-arrows"
+          >
+          </v-icon>
         </div>
       </div>
     </v-img>
-    <v-container
-      class="d-flex flex-column justify-center align-center"
-      style="background-color: rgba(137, 119, 99, 0.784)"
-    >
-      <v-btn
-        block
-        variant="outlined"
-        href="https://www.google.com/maps?saddr=My+Location&daddr=Chiesa+Marigolda"
-        class="mb-4"
-      >
-        <v-icon icon="fas fa-route" size="large" class="mr-8"> </v-icon>
-        Dov'è il matrimonio?
-      </v-btn>
-      <v-btn
-        block
-        variant="outlined"
-        href="https://www.google.com/maps?saddr=My+Location&daddr=Agriturismo+Larice+Clusone"
-      >
-        <v-icon icon="fas fa-route" size="large" class="mr-8"> </v-icon>
-        Dov'è il ricevimento?
-      </v-btn>
-    </v-container>
+
+    <Directions />
   </v-container>
 </template>
+
+<script setup lang="ts">
+import Directions from "../components/Directions.vue";
+</script>
 
 <script lang="ts">
 export default {
@@ -62,5 +50,23 @@ export default {
 <style lang="css" scoped>
 .w-100 {
   width: 100%;
+}
+
+.moving-arrows {
+  animation: updown 2s ease infinite;
+}
+
+@keyframes updown {
+  0% {
+    transform: translateY(-20%);
+  }
+
+  50% {
+    transform: translateY(20%);
+  }
+
+  100% {
+    transform: translateY(-20%);
+  }
 }
 </style>
