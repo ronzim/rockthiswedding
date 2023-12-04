5faa5b252b02037db85a290e85ccc1958b28cf0d
mattia-wsl <mat.ronzoni@gmail.com>
2023-12-04 22:38:04 +0100

    fix accordion and add rsvp form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <router-view />
-  <!-- <h1>prova</h1> -->
 </template>
 
 <script lang="ts" setup>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,20 +1,19 @@
 <template>
   <v-container
-    class="d-flex flex-column justify-center align-center"
-    style="background-color: rgba(94, 119, 69, 0.7); max-width: none"
+    class="d-flex flex-column justify-center align-center pa-0"
+    style="max-width: none"
   >
     <!-- accordion -->
-    <v-expansion-panels accordion>
+    <v-expansion-panels variant="accordion">
       <!-- Cerimonia -->
-      <v-expansion-panel
-        style="background-color: rgba(94, 119, 69, 0.7)"
-        @click="scrollIntoView"
-      >
-        <v-expansion-panel-title>
+      <v-expansion-panel @click="scrollIntoView" elevation="0">
+        <v-expansion-panel-title disable-icon-rotate>
           <h3>Cerimonia</h3>
-          <v-icon icon="fas fa-church" size="small" class="ml-4"> </v-icon>
+          <template v-slot:actions>
+            <v-icon icon="fas fa-church"> </v-icon>
+          </template>
         </v-expansion-panel-title>
-        <v-expansion-panel-text>
+        <v-expansion-panel-text class="text-center">
           Chiesa dell'Annunciazione <br />
           Via Abruzzi (Marigolda), Curno
           <v-btn
@@ -30,15 +29,14 @@
         </v-expansion-panel-text>
       </v-expansion-panel>
       <!-- Ricevimento -->
-      <v-expansion-panel
-        style="background-color: rgba(94, 119, 69, 0.7)"
-        @click="scrollIntoView"
-      >
-        <v-expansion-panel-title>
+      <v-expansion-panel @click="scrollIntoView" elevation="0">
+        <v-expansion-panel-title disable-icon-rotate>
           <h3>Ricevimento</h3>
-          <v-icon icon="fas fa-utensils" size="small" class="ml-4"> </v-icon>
+          <template v-slot:actions>
+            <v-icon icon="fas fa-utensils"> </v-icon>
+          </template>
         </v-expansion-panel-title>
-        <v-expansion-panel-text>
+        <v-expansion-panel-text class="text-center">
           Agriturismo Larice <br />
           Strada per San Lucio, Clusone
           <v-btn
@@ -53,19 +51,47 @@
           </v-btn>
         </v-expansion-panel-text>
       </v-expansion-panel>
-      <!-- Regalo -->
-      <v-expansion-panel
-        style="background-color: rgba(94, 119, 69, 0.7)"
-        @click="scrollIntoView"
-      >
-        <v-expansion-panel-title>
-          <h3>Lista nozze</h3>
-          <v-icon icon="fas fa-gift" size="small" class="ml-4"> </v-icon>
+      <!-- Conferma -->
+      <v-expansion-panel @click="scrollIntoView" elevation="0">
+        <v-expansion-panel-title disable-icon-rotate>
+          <h3>RSVP</h3>
+          <template v-slot:actions>
+            <v-icon icon="fas fa-check"> </v-icon>
+          </template>
         </v-expansion-panel-title>
-        <v-expansion-panel-text>
+        <v-expansion-panel-text class="text-center">
+          <v-btn
+            block
+            variant="outlined"
+            href="https://forms.gle/M7P9UHTyr1pn7Ha86"
+            class="my-2"
+          >
+            Vai alla conferma
+            <v-icon icon="fas fa-arrow-right" size="small" class="ml-4">
+            </v-icon>
+          </v-btn>
+          <!-- <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfl7U6OkxxPzcsNUaOefdC6zPKINydfhbRTq9nEWYUlQhQ7rA/viewform?embedded=true"
+            width="640"
+            height="776"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            >Caricamento…</iframe
+          > -->
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+      <!-- Regalo -->
+      <v-expansion-panel @click="scrollIntoView" elevation="0">
+        <v-expansion-panel-title disable-icon-rotate>
+          <h3>Regalo</h3>
+          <template v-slot:actions>
+            <v-icon icon="fas fa-gift"> </v-icon>
+          </template>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text class="text-center">
           IBAN: IT 00 A 00000 00000 000000000000 <br />
           Intestato a: Lisa & Mattia <br />
-          Causale: Lista nozze
         </v-expansion-panel-text>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -111,3 +137,13 @@ export default {
   },
 };
 </script>
+
+<style lang="css" scoped>
+.v-expansion-panel-header {
+  padding: 0px 24px !important;
+}
+.v-expansion-panel {
+  border-radius: 0% !important;
+  background-color: rgba(42, 109, 196, 0.564);
+}
+</style>
